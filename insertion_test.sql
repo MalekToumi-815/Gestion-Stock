165cc188a68abe4bf9eb30200ce91d79bec65819
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2025-06-23 10:14:58 +0100

    insertion

---
+++ insertion_test.sql
@@ -0,0 +1,12 @@
+-- 1. Insert into Utilisateurs
+INSERT INTO Utilisateurs (nom, prenom, login, mdp)
+VALUES ('Toumi', 'Malek', 'malek_admin', 'admin123');
+
+-- 2. Get the last inserted matricule (if you're using MySQL CLI)
+SELECT LAST_INSERT_ID();  -- Let’s say it returns 1
+
+-- 3. Insert into Administrateurs
+INSERT INTO Administrateurs (id_admin)
+VALUES (1);
+INSERT INTO Utilisateurs (nom, prenom, login, mdp)
+VALUES ('Ben Ali', 'Sami', 'sami_user', 'user123');
